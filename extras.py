86f598023fcdafec13c9ffa82ba54cf57c7fa51d
Matteo Cantiello <matteo.cantiello@gmail.com>
2023-03-14 12:44:26 -0400

    added calculations

---
+++ extras.py
@@ -0,0 +1,124 @@
+import matplotlib.pyplot as plt
+import mesa_reader as mr
+import numpy as np
+from pylab import *
+from math import log10, pi
+from matplotlib import rc
+#rc('mathtext', default='regular')
+import matplotlib as mpl
+import matplotlib.tri as tri
+import pandas as pd 
+import pickle
+
+import matplotlib.transforms as mtransforms
+from matplotlib.ticker import MaxNLocator
+from scipy import interpolate
+from scipy.interpolate import griddata
+from scipy.interpolate import interp2d
+
+
+
+from numpy import loadtxt
+import pandas as pd 
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+ 
+from Constants import Constants
+c=Constants() 
+
+# Define some colors
+BLACK      = (0.00,0.00,0.00)
+ORANGE     = (0.90,0.60,0.00)
+SKY_BLUE   = (0.35,0.70,0.90)
+BLUE_GREEN = (0.00,0.60,0.50)
+YELLOW     = (0.95,0.90,0.25)
+BLUE       = (0.00,0.45,0.70)
+VERMILLION = (0.80,0.40,0.00)
+RED_PURPLE = (0.80,0.60,0.70)
+
+####################################################
+# Plt Options from Perna, Lazzati & Cantiello 2017 #
+####################################################
+plt.rcParams['figure.figsize'] = (7,6)
+plt.rcParams['font.family'] = ['Times New Roman']
+#plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams['mathtext.fontset'] = 'dejavusans'
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['mathtext.rm'] = 'serif'
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['xtick.labelsize'] = 20
+plt.rcParams['ytick.labelsize'] = 20
+plt.rcParams['axes.titlepad'] = 20 
+plt.rcParams['legend.frameon'] = False
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['font.size'] = 16
+plt.rcParams['legend.borderpad'] = 0.2
+plt.rc('legend', borderpad=0.2, markerscale=1., fancybox=False)
+plt.rcParams['legend.labelspacing'] = 0.5
+plt.rcParams['legend.handletextpad'] = 0.5
+plt.rcParams['legend.handlelength'] = 1
+plt.rcParams["legend.framealpha"] = 1.0
+plt.rcParams["contour.negative_linestyle"]='solid'
+####################################################
+
+############### SOME DEFINITIONS
+rsun=6.9598e10
+msun=1.9892e33
+lsun=3.8418e33
+tsun = 5777
+G=6.67428e-8 
+ell_sun=(5777)**4.0/(274*100)  # Used for Spectroscopic HRD 
+alpha_mlt=1.6 
+lgteff = r'$\log\, $T$_{\rm eff}/$K'
+lgl= r'$\log\, L / L_\odot$'
+lgell= r'$\log\, \mathscr{L} / \mathscr{L}_\odot$'
+
+### SOME FUNCTIONS
+
+def find_zams(logl,loglh,model):
+    zams=1
+    while (loglh[zams] < 1.0*logl[zams]): 
+     zams=zams+1
+    return zams; 
+
+def find_h(dh,center_h1,model):
+    zams=1
+    while (center_h1[zams] > (center_h1[1] - dh)): 
+     zams=zams+1
+    return zams; 
+    
+def find_tams(center_h1,model):
+    tams=1
+    n=len(model)
+    while (center_h1[tams] > 0.05) and (tams < n-1): 
+     tams=tams+1
+    return tams;    
+
+def find_max(a,b,c,d):
+    z= [0] * len(a)
+    for i in range(0, len(a)):
+      z[i]=max(a[i],b[i],c[i],d[i])   
+    return z;
+
+def binary(z):  
+    for i in range(0, len(z)):
+      if z[i] > 0:
+         z[i] = 1
+      else: 
+         z[i] = 0
+    return z;
+
+def pbeta(p,b):    # Plasma Beta
+    beta= 8*3.1415*p/(b**2.0)
+    return beta;
+
+def rossby(vcon,vroteq,hp,req): # Rossby number, defined as Prot/Pcon
+    ross =(3.14*req/hp)*(vcon/veq)
+    return ross; 
+def beq(vcon,rho): # Calculate equipartition Bfield
+    b=vcon*(4*3.14*rho)**0.5
+    return b;
+def char_freq(vcon,hp,req): # Rossby number, defined as Prot/Pcon
+    ross =(3.14*req/hp)*(vcon/veq)
+    return ross; 
